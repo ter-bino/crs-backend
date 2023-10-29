d558678526ebf0848629923298da0f3751f960fa
John Kelly <johnkellytemena@gmail.com>
2023-10-29 15:31:42 +0800

    added more tables without foreign key

---
+++ database/migrations/2023_10_29_072041_create_subject_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('subject', function (Blueprint $table) {
+            $table->id('subject_id');
+            $table->string('subject_code');
+            $table->string('subject_title');
+            $table->string('subject_type');
+            $table->int('units');
+            $table->int('credited_units');
+            $table->boolean('active_status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('subject');
+    }
+};
