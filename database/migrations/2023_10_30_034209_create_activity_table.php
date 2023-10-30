27177397056c8f218b12b010f901a9f4aaf513f5
John Kelly <johnkellytemena@gmail.com>
2023-10-30 11:48:01 +0800

    created activity table

---
+++ database/migrations/2023_10_30_034209_create_activity_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('activity', function (Blueprint $table) {
+            $table->id('activity_id');
+            $table->foreignId('activity_type_id')->constrained(
+                table: 'activity_type', column: 'activity_type_id'
+            );
+            $table->year('academic_year');
+            $table->integer('term');
+            $table->date('start_date');
+            $table->time('start_time');
+            $table->date('end_date');
+            $table->time('end_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('activity');
+    }
+};
