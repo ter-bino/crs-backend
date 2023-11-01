f56f0448dcc5a1ac12b5ee72fb56cb6537846da0
RekhaelaNvrr <navarro.rekhaela@gmail.com>
2023-11-01 22:56:20 +0800

    created dropped classes table

---
+++ database/migrations/2023_11_01_145325_create_dropped_classes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('dropped_classes', function (Blueprint $table) {
+            $table->foreignId('add_drop_request_id')->constrained(
+                table: 'add_drop_request', column: 'add_drop_request_id'
+            );
+            $table->foreignId('class_id')->constrained(
+                table: 'class', column: 'class_id'
+            );
+
+            # create composite key
+            $table->primary(['add_drop_request_id', 'class_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('dropped_classes');
+    }
+};
