f85c0d34b3d13ea49baf58eafab050c4c64fb723
John Kelly <johnkellytemena@gmail.com>
2023-11-05 20:36:42 +0800

    created student balance model

---
+++ app/Models/StudentBalance.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class StudentBalance extends Model
+{
+    use HasFactory;
+
+    public $timestamps = true; // automatically create created_at and updated_at timestamps for each model
+    protected $table = 'student_balance'; // declares the associated table
+    protected $primaryKey = 'student_balance_id'; // declares the column name to be used for id
+
+    protected $fillable = [
+        'terms_of_payment',
+        'assessment_type',
+        'academic_year',
+        'term',
+        'total_amount',
+        'paid_amount',
+        'overall_paid',
+        'overall_balance'
+    ];
+
+    protected $casts = [
+        'term' => 'integer',
+        'total_amount' => 'decimal:12,2',
+        'paid_amount' => 'decimal:12,2',
+        'overall_paid' => 'decimal:12,2',
+        'overall_balance' => 'decimal:12,2'
+    ];
+}
