03bfcf39679e3d8982381ad5f8658b4c9a9116ee
Alter Orbino Jr. <alter.orbino@technomancer.biz>
2023-11-08 03:59:26 +0800

    relations/instructor to teach assignment and department

--- app/Models/Instructor.php
+++ app/Models/Instructor.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Instructor extends Model
 {
@@ -22,5 +24,15 @@ class Instructor extends Model
     public function staff_info(): BelongsTo
     {
         return $this->belongsTo(Program::class, 'staff_id', 'staff_id');
+    }
+
+    public function teaching_assignments(): HasMany
+    {
+        return $this->hasMany(TeachingAssignment::class, 'instructor_id', 'instructor_id');
+    }
+
+    public function departments(): BelongsToMany
+    {
+        return $this->belongsToMany(Department::class, 'instructor_department_assignment', 'instructor_id', 'department_id');
     }
 }
