d1ea41afd47e6b8b05b3e1510a7e3f78bca359e5
Alter Orbino Jr. <alter.orbino@technomancer.biz>
2023-11-08 03:41:31 +0800

    relations/program to other tables

--- app/Models/Program.php
+++ app/Models/Program.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Program extends Model
 {
@@ -24,5 +26,20 @@ class Program extends Model
     public function college(): BelongsTo
     {
         return $this->belongsTo(College::class, 'college_id','college_id');
+    }
+
+    public function student_terms(): HasMany
+    {
+        return $this->hasMany(StudentTerm::class, 'program_id', 'program_id');
+    }
+
+    public function department(): HasOne
+    {
+        return $this->hasOne(Department::class, 'program_id', 'program_id');
+    }
+
+    public function blocks(): HasMany
+    {
+        return $this->hasMany(Block::class, 'program_id', 'program_id');
     }
 }
