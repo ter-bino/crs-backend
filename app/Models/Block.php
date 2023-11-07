a9a516b57a16efeffce8ad1114683f7e598a9cc4
Alter Orbino Jr. <alter.orbino@technomancer.biz>
2023-11-08 03:44:06 +0800

    relations/block to other tables

--- app/Models/Block.php
+++ app/Models/Block.php
@@ -4,7 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Block extends Model
 {
@@ -20,13 +22,23 @@ class Block extends Model
         'slots'
     ];
     
-    public function program()
+    public function program(): BelongsTo
     {
         return $this->belongsTo(Program::class, 'program_id', 'program_id');
+    }
+
+    public function student_terms(): HasMany
+    {
+        return $this->hasMany(StudentTerm::class, 'block_id', 'block_id');
     }
 
     public function students(): BelongsToMany
     {
         return $this->belongsToMany(Student::class, 'block_student_assignment', 'block_id', 'student_id');
+    }
+
+    public function classes(): BelongsToMany
+    {
+        return $this->belongsToMany(M_Class::class, 'block_class_assignment', 'block_id', 'class_id');
     }
 }
