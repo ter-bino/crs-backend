5ae75dc58d62c9bbe9f0bd77880a41fcdae28283
Alter Orbino Jr. <alter.orbino@technomancer.biz>
2023-11-08 04:10:06 +0800

    relations/ teaching_assignment to other tables

--- app/Models/TeachingAssignment.php
+++ app/Models/TeachingAssignment.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class TeachingAssignment extends Model
 {
@@ -17,4 +20,24 @@ class TeachingAssignment extends Model
         'term',
         'start_date',
     ];
+
+    public function instructor(): BelongsTo
+    {
+        return $this->belongsTo(Instructor::class, 'instructor_id', 'instructor_id');
+    }
+
+    public function consultation_hours(): HasMany
+    {
+        return $this->hasMany(ConsultationHour::class, 'teachinh_assignment_id', 'teaching_assignment_id');
+    }
+
+    public function load_types(): BelongsToMany
+    {
+        return $this->belongsToMany(LoadType::class, 'faculty_class_assignment', 'teaching_assignment_id', 'load_type_id');
+    }
+
+    public function classes(): BelongsToMany
+    {
+        return $this->belongsToMany(M_class::class, 'faculty_class_assignment', 'teaching_assignment_id', 'class_id');
+    }
 }
