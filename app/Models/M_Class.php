6ba2dabd00880be2b1239565fa92af91a057bf9a
Alter Orbino Jr. <alter.orbino@technomancer.biz>
2023-11-08 04:25:31 +0800

    relations/Class to other tables

--- app/Models/M_Class.php
+++ app/Models/M_Class.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class M_Class extends Model
 {
@@ -25,8 +26,38 @@ class M_Class extends Model
         'active_status'
     ];
 
+    public function subject(): BelongsTo
+    {
+        return $this->belongsTo(Subject::class, 'subject_id', 'subject_id');
+    }
+
     public function instruction_language(): BelongsTo
     {
         return $this->belongsTo(InstructionLanguage::class, 'instruction_language_id', 'instruction_language_id');
+    }
+
+    public function teachingAssignments(): BelongsToMany
+    {
+        return $this->belongsToMany(TeachingAssignment::class, 'faculty_class_assignment', 'class_id', 'teaching_assignment_id');
+    }
+
+    public function students(): BelongsToMany
+    {
+        return $this->belongsToMany(Student::class, 'class_student_assignment', 'class_id', 'student_id');
+    }
+
+    public function blocks(): BelongsToMany
+    {
+        return $this->belongsToMany(Block::class, 'block_class_assignment', 'class_id', 'block_id');
+    }
+
+    public function class_drops(): BelongsToMany
+    {
+        return $this->belongsToMany(AddDropRequest::class, 'dropped_classes', 'class_id', 'add_drop_request_id');
+    }
+
+    public function class_adds(): BelongsToMany
+    {
+        return $this->belongsToMany(AddDropRequest::class, 'added_classes', 'class_id', 'add_drop_request_id');
     }
 }
