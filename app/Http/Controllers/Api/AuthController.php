86b8e40c3a8d4d82449b8c6d292ab8ec2c43ffe8
Alter Orbino Jr. <alter.orbino@technomancer.biz>
2023-11-20 14:14:10 +0800

    api for logout

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -58,6 +58,22 @@ class AuthController extends Controller
         ])->cookie($cookie);
     }
 
+    public function logout() {
+        $sessionToken = Cookie::get('CRS-API-SESSION-TOKEN');
+
+        if($sessionToken) {
+            $userToken = UserToken::where('user_session_token', $sessionToken)->first();
+
+            if($userToken) {
+                $userToken->delete();
+            }
+        }
+
+        return response()->json([
+            'message' => 'Successful logout'
+        ])->withCookie(Cookie::forget('CRS-API-SESSION-TOKEN'));
+    }
+
     protected function getMicrosoftUser($accessToken)
     {
         return Socialite::driver('azure')->userFromToken($accessToken);
